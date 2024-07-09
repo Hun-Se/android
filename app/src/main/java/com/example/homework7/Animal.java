229cc150f3fb02908046f5c2eb262da240b6927c
Hun-Se <kim72840@gmail.com>
2024-07-09 20:56:06 +0900

    first commit

---
+++ app/src/main/java/com/example/homework7/Animal.java
@@ -0,0 +1,19 @@
+package com.example.homework7;
+
+public class Animal {
+    String name;
+    int age;
+    String phone;
+
+    Animal (String name, int age, String phone) {
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/homework7/Cat.java
@@ -0,0 +1,4 @@
+package com.example.homework7;
+
+public class Cat {
+}
